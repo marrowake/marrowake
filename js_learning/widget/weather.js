1bc39849716c3209a6860d1e1ab9540dc83c914a
reporeaper <aupontrouge2018@gmail.com>
2022-04-26 15:53:44 +0300

    end of tutorial

--- js_learning/widget/weather.js
+++ js_learning/widget/weather.js
@@ -1,4 +1,19 @@
 const COORDS_LS = 'coords'
+const API_KEY = 'e853ca1f327cc45d83d2598f9d475f58' //https://home.openweathermap.org/api_keys password=NA7JrVbF
+
+const weatherContainer = document.querySelector('.js-weather')
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(json) {
+            const temperature = json.main.temp
+            const place = json.name
+            weatherContainer.innerText = `${temperature} @ ${place}`
+        })
+}
 
 function saveCoords(positionObject) {
     localStorage.setItem(COORDS_LS, JSON.stringify(positionObject))
@@ -12,6 +27,7 @@ function geoSuccessHandler(position) {
         longitude
     }
     saveCoords(positionObject)
+    getWeather(latitude, longitude)
 }
 
 function geoErrorHandler() {
@@ -27,7 +43,8 @@ function getCoords() {
     if (coords === null) {
         askForCoords()
     } else {
-        //getWeather func
+        const loadedCoords = JSON.parse(coords)
+        getWeather(loadedCoords.latitude, loadedCoords.longitude)
     }
 }
 
